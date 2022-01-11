8ee65a05f998d07dcd122e9d3b1e41b2419a993b
JY <devetoile@gmail.com>
2022-01-11 23:48:25 +0900

    fragement

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 function App() {
+  const name = "react";
   return (
-    <div>
-      <h1>리액트야 안녕</h1>
+    <>
+      <h1>{name} 리액트야 안녕</h1>
       <h2>잘작동하니?</h2>
-    </div>
+    </>
   );
 }
 
